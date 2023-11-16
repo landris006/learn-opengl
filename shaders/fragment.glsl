6024e7504824eb701416b898ed98a227832cab4e
landris006 <laczko.andris007@gmail.com>
2023-11-16 23:50:45 +0100

    3d

--- shaders/fragment.glsl
+++ shaders/fragment.glsl
@@ -1,6 +1,7 @@
 #version 330 core
-in vec2 TexCoord;
+in vec2 texCoord;
 in vec4 position;
+
 out vec4 FragColor;
 
 uniform sampler2D texture1;
@@ -8,8 +9,8 @@ uniform sampler2D texture2;
 
 void main() {
   FragColor = mix(
-    texture(texture1, TexCoord),
-    texture(texture2, vec2(-2.0 * TexCoord.x, 2.0 * TexCoord.y)),
+    texture(texture1, texCoord),
+    texture(texture2, vec2(-2.0 * texCoord.x, 2.0 * texCoord.y)),
     0.5
   ) * (position + 1) / 2;
 }
--- src/lib.cpp
+++ src/lib.cpp
@@ -1,3 +1,5 @@
+#include "glm/ext/matrix_clip_space.hpp"
+#include <sys/types.h>
 #define GLFW_INCLUDE_NONE
 #include "glm/ext/matrix_float4x4.hpp"
 #include "glm/ext/matrix_transform.hpp"
@@ -19,28 +21,50 @@ void processInput(GLFWwindow *window) {
 
 struct Vertex {
   glm::vec3 position;
-  glm::vec3 color;
-  glm::vec2 texCoords;
+  glm::vec2 texCoord;
 };
 
-Vertex vertices[] = {
-    {.position = {0.5f, 0.5f, 0.0f},
-     .color = {1.0f, 0.0f, 0.0f},
-     .texCoords = {1.0, 1.0}},
-    {.position = {0.5f, -0.5f, 0.0f},
-     .color = {0.0f, 1.0f, 0.0f},
-     .texCoords = {1.0, 0.0}},
-    {.position = {-0.5f, -0.5f, 0.0f},
-     .color = {0.0f, 0.0f, 1.0f},
-     .texCoords = {0.0, 0.0}},
-    {.position = {-0.5f, 0.5f, 0.0f},
-     .color = {1.0f, 1.0f, 0.0f},
-     .texCoords = {0.0, 1.0}}};
-unsigned int indices[] = {3, 2, 1, 3, 1, 0};
+// Vertex vertices[] = {
+//     {.position = {0.5f, 0.5f, 0.0f}, .texCoord = {1.0, 1.0}},
+//     {.position = {0.5f, -0.5f, 0.0f}, .texCoord = {1.0, 0.0}},
+//     {.position = {-0.5f, -0.5f, 0.0f}, .texCoord = {0.0, 0.0}},
+//     {.position = {-0.5f, 0.5f, 0.0f}, .texCoord = {0.0, 1.0}}};
+// unsigned int indices[] = {3, 2, 1, 3, 1, 0};
+float vertices[] = {
+    -0.5f, -0.5f, -0.5f, 0.0f,  0.0f,  0.5f,  -0.5f, -0.5f, 1.0f,  0.0f,  0.5f,
+    0.5f,  -0.5f, 1.0f,  1.0f,  0.5f,  0.5f,  -0.5f, 1.0f,  1.0f,  -0.5f, 0.5f,
+    -0.5f, 0.0f,  1.0f,  -0.5f, -0.5f, -0.5f, 0.0f,  0.0f,  -0.5f, -0.5f, 0.5f,
+    0.0f,  0.0f,  0.5f,  -0.5f, 0.5f,  1.0f,  0.0f,  0.5f,  0.5f,  0.5f,  1.0f,
+    1.0f,  0.5f,  0.5f,  0.5f,  1.0f,  1.0f,  -0.5f, 0.5f,  0.5f,  0.0f,  1.0f,
+    -0.5f, -0.5f, 0.5f,  0.0f,  0.0f,  -0.5f, 0.5f,  0.5f,  1.0f,  0.0f,  -0.5f,
+    0.5f,  -0.5f, 1.0f,  1.0f,  -0.5f, -0.5f, -0.5f, 0.0f,  1.0f,  -0.5f, -0.5f,
+    -0.5f, 0.0f,  1.0f,  -0.5f, -0.5f, 0.5f,  0.0f,  0.0f,  -0.5f, 0.5f,  0.5f,
+    1.0f,  0.0f,  0.5f,  0.5f,  0.5f,  1.0f,  0.0f,  0.5f,  0.5f,  -0.5f, 1.0f,
+    1.0f,  0.5f,  -0.5f, -0.5f, 0.0f,  1.0f,  0.5f,  -0.5f, -0.5f, 0.0f,  1.0f,
+    0.5f,  -0.5f, 0.5f,  0.0f,  0.0f,  0.5f,  0.5f,  0.5f,  1.0f,  0.0f,  -0.5f,
+    -0.5f, -0.5f, 0.0f,  1.0f,  0.5f,  -0.5f, -0.5f, 1.0f,  1.0f,  0.5f,  -0.5f,
+    0.5f,  1.0f,  0.0f,  0.5f,  -0.5f, 0.5f,  1.0f,  0.0f,  -0.5f, -0.5f, 0.5f,
+    0.0f,  0.0f,  -0.5f, -0.5f, -0.5f, 0.0f,  1.0f,  -0.5f, 0.5f,  -0.5f, 0.0f,
+    1.0f,  0.5f,  0.5f,  -0.5f, 1.0f,  1.0f,  0.5f,  0.5f,  0.5f,  1.0f,  0.0f,
+    0.5f,  0.5f,  0.5f,  1.0f,  0.0f,  -0.5f, 0.5f,  0.5f,  0.0f,  0.0f,  -0.5f,
+    0.5f,  -0.5f, 0.0f,  1.0f};
+
+glm::vec3 cubePositions[] = {
+    glm::vec3(0.0f, 0.0f, 0.0f),
+    glm::vec3(2.0f, 5.0f, -15.0f),
+    glm::vec3(-1.5f, -2.2f, -2.5f),
+    glm::vec3(-3.8f, -2.0f, -12.3f),
+    glm::vec3(2.4f, -0.4f, -3.5f),
+    glm::vec3(-1.7f, 3.0f, -7.5f),
+    glm::vec3(1.3f, -2.0f, -2.5f),
+    glm::vec3(1.5f, 2.0f, -2.5f),
+    glm::vec3(1.5f, 0.2f, -1.5f),
+    glm::vec3(-1.3f, 1.0f, -1.5f)};
 
 void render(const Shader &shaderProgram) {
   glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
   glClear(GL_COLOR_BUFFER_BIT);
+  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
   unsigned int vertexArrayObject;
   glGenVertexArrays(1, &vertexArrayObject);
@@ -56,57 +80,54 @@ void render(const Shader &shaderProgram) {
 
   glVertexAttribPointer(
       1,
-      3,
+      2,
       GL_FLOAT,
       GL_FALSE,
       sizeof(Vertex),
-      (const void *)(offsetof(Vertex, color)));
+      (const void *)(offsetof(Vertex, texCoord)));
   glEnableVertexAttribArray(1);
-
-  glVertexAttribPointer(
-      2,
-      2,
-      GL_FLOAT,
-      GL_FALSE,
-      sizeof(Vertex),
-      (const void *)(offsetof(Vertex, texCoords)));
-  glEnableVertexAttribArray(2);
 
   unsigned int vertexBufferObject;
   glGenBuffers(1, &vertexBufferObject);
   glBindBuffer(GL_ARRAY_BUFFER, vertexBufferObject);
   glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-  unsigned int elementBufferObject;
-  glGenBuffers(1, &elementBufferObject);
-  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, elementBufferObject);
-  glBufferData(
-      GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+  // unsigned int elementBufferObject;
+  // glGenBuffers(1, &elementBufferObject);
+  // glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, elementBufferObject);
+  // glBufferData(
+  //     GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
   shaderProgram.use();
   shaderProgram.setInt("texture1", 0);
   shaderProgram.setInt("texture2", 1);
 
-  glm::mat4 transform = glm::mat4(1.0f);
-  transform = glm::translate(
-      transform,
-      glm::vec3(
-          sin(glfwGetTime() + 3.14 / 2) / 2, sin(glfwGetTime()) / 2, 0.0));
-  transform = glm::rotate(
-      transform, static_cast<float>(glfwGetTime()), glm::vec3(0.0, 0.0, 1.0));
-  transform = glm::scale(
-      transform, static_cast<float>(sin(glfwGetTime())) * glm::vec3(1.0));
-  // transform =
-  //     glm::rotate(transform, (float)glfwGetTime(), glm::vec3(0.0, 1.0, 0.0));
-  // transform =
-  //     glm::rotate(transform, (float)glfwGetTime(), glm::vec3(1.0, 0.0, 0.0));
-
-  shaderProgram.setMatrix4("transform", transform);
-
-  // glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-  glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+  glm::mat4 view = glm::mat4(1.0f);
+  // note that we’re translating the scene in the reverse direction
+  view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
+
+  glm::mat4 projection;
+  projection =
+      glm::perspective(glm::radians(90.0f), 800.0f / 600.0f, 0.1f, 100.0f);
+
+  shaderProgram.setMatrix4("view", view);
+  shaderProgram.setMatrix4("projection", projection);
+
+  for (uint i = 0; i < 10; i++) {
+    glm::mat4 model = glm::mat4(1.0f);
+    model = glm::translate(model, cubePositions[i]);
+    float angle = 20.0f * i + (i % 3 == 0 ? glfwGetTime() * 10 : 0);
+    model =
+        glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
+
+    shaderProgram.setMatrix4("model", model);
+
+    // glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+
+    glDrawArrays(GL_TRIANGLES, 0, 36);
+  }
 
   glBindVertexArray(0);
 }
@@ -202,6 +223,8 @@ int run() {
   glTexParameteri(
       GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+  glEnable(GL_DEPTH_TEST);
 
   loadTextures();
   loadTextures();
